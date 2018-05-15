96137458fefe6ca2e481ce74bd002f80f4ac69fb
Nicholas Crossley <ndjc@ncrossley.com>
2018-05-15 19:19:20 +0000

    Add test for removeFragment
    git-svn-id: https://tools.oasis-open.org/version-control/svn/oslc-core/trunk@509 ebe71ad9-cacf-4371-9415-6204173e009e

---
+++ tools/ShapeChecker/test/net/open_services/scheck/shapechecker/HttpHandlerTest.java
@@ -0,0 +1,32 @@
+package net.open_services.scheck.shapechecker;
+
+import java.net.URISyntaxException;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for HttpHandlerTest. *
+ * @author Nick Crossley. Released to public domain 2018.
+ */
+public class HttpHandlerTest
+{
+    private HttpHandler httpHandler = new HttpHandler();
+
+    /**
+     * Test removeFragment method.
+     * @throws URISyntaxException if something goes wrong
+     */
+    @Test
+    public void testRemoveFragment() throws URISyntaxException
+    {
+        assertEquals(
+            "http://open-services.net/ns/core",
+            httpHandler.removeFragment("http://open-services.net/ns/core#name").toString());
+        assertEquals(
+            "http://open-services.net/ns/core",
+            httpHandler.removeFragment("http://open-services.net/ns/core#").toString());
+    }
+
+}
