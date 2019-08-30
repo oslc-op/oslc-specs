70de2b53e110867f776dd5457258803009afa386
Andrew Berezovskyi <andriib@kth.se>
2019-08-30 21:46:31 +0200

    Ensure CM & Core vocabs are not fetched

--- tools/ShapeChecker/check-cm.sh
+++ tools/ShapeChecker/check-cm.sh
@@ -6,6 +6,9 @@
 
 
 build/install/ShapeChecker/bin/ShapeChecker \
+-x http://open-services.net/ns/core ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
+-x http://open-services.net/ns/cm ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
+-v ../../specs/core/vocab/core-vocab.ttl \
 -v ../../specs/cm/change-mgt-vocab.ttl \
 -s ../../specs/cm/change-mgt-shapes.ttl
 
