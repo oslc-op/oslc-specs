f10337d403cd70fc452f33a3ff1e580ea3a5e92e
Andrew Berezovskyi <andriib@kth.se>
2019-08-30 21:22:16 +0200

    Initial CM check script

--- tools/ShapeChecker/check-cm.sh
+++ tools/ShapeChecker/check-cm.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-exit 0
+
+# -x http://open-services.net/ns/core ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
+# -x http://open-services.net/ns/core\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
+
+
+build/install/ShapeChecker/bin/ShapeChecker \
+-v ../../specs/cm/change-mgt-vocab.ttl \
+-v ../../specs/cm/change-mgt-shapes.ttl
+
+exit 1
