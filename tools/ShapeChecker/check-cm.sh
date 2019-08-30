70534be52c4bbdbf50e500a919b63f71535766c3
Andrew Berezovskyi <andriib@kth.se>
2019-08-30 21:48:11 +0200

    Clean up the config

--- tools/ShapeChecker/check-cm.sh
+++ tools/ShapeChecker/check-cm.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-
-# -x http://open-services.net/ns/core ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
-# -x http://open-services.net/ns/core\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
-
-
 build/install/ShapeChecker/bin/ShapeChecker \
 -x http://open-services.net/ns/core ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
 -x http://open-services.net/ns/cm ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
 -v ../../specs/core/vocab/core-vocab.ttl \
 -v ../../specs/cm/change-mgt-vocab.ttl \
 -s ../../specs/cm/change-mgt-shapes.ttl
-
-exit 1
