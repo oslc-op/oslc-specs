9083f1555e590cb8c83797c429fae7b6d0d3b6d6
Andrew Berezovskyi <andriib@kth.se>
2019-09-05 17:07:56 +0200

    Reorganise RDF files for the RM spec (#119)
    * Reorganise RDF files for the RM spec
    
    - rm.xml and rm.ttl were isomorphic. XML was removed and Turtle was 
    formatted with Jena RIOT formatter into rm_jra.ttl
    - rm.rdf was renamed into rm_2.0.xml (for history) and converted into 
    Turtle file rm_2.0.ttl
    - the current vocab file was reformatted.
    
    * Add OSLC RM shape & vocab checks
    
    * Update of the "inverse" properties avoid using the word "inverse".

---
+++ tools/ShapeChecker/check-rm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+build/install/ShapeChecker/bin/ShapeChecker \
+-x http://open-services.net/ns/core ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
+-x http://open-services.net/ns/core\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
+-x http://open-services.net/ns/rm ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
+-x http://open-services.net/ns/rm\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
+-v ../../specs/rm/requirements-management-vocab.ttl \
+-s ../../specs/rm/requirements-management-shapes.ttl \
