d31f75ec51bf3dd5b5c2df8e4ffc248976c38a27
Andrew Berezovskyi <andriib@kth.se>
2020-06-04 21:20:39 +0200

    Fix SC vocabulary fetching (#322)
    * Force proper headers in redirects for SC
    * Add more debug logging for reading RDF
    * Full fix for Dublin Core
    * Implement our own redirect strategy to handle 308
    * Bring back dcterms checks
    
    Co-authored-by: Nick Crossley <ndjc@ncrossley.com>

--- tools/ShapeChecker/check-rm.sh
+++ tools/ShapeChecker/check-rm.sh
@@ -5,6 +5,5 @@ build/install/ShapeChecker/bin/ShapeChecker \
 -x http://open-services.net/ns/core\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
 -x http://open-services.net/ns/rm ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
 -x http://open-services.net/ns/rm\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
--x 'https?://purl.org/dc/terms.*' ${comment# to address dcterms content negotiation problem} \
 -v ../../specs/rm/requirements-management-vocab.ttl \
 -s ../../specs/rm/requirements-management-shapes.ttl \
