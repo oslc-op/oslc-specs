c1238c2ff8532fb06a537de9dd0ca611b9daba0b
Andrew Berezovskyi <andriib@kth.se>
2020-06-04 13:52:01 +0200

    Extend the fix to the core SC script

--- tools/ShapeChecker/check-core.sh
+++ tools/ShapeChecker/check-core.sh
@@ -3,6 +3,7 @@
 build/install/ShapeChecker/bin/ShapeChecker \
 -x http://open-services.net/ns/core ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
 -x http://open-services.net/ns/core\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
+-x 'https?://purl.org/dc/terms.*' ${comment# to address dcterms content negotiation problem} \
 -v ../../specs/core/vocab/core-vocab.ttl \
 -s ../../specs/core/shapes/AttachmentDescriptor-shape.ttl \
 -s ../../specs/core/shapes/Comment-shape.ttl \
