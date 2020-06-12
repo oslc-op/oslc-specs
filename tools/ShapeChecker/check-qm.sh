5f87732d06da6056eb11a13002118fc3b70244fe
Andrew Berezovskyi <andriib@kth.se>
2020-06-12 16:19:45 +0200

    Complete the workaround

--- tools/ShapeChecker/check-qm.sh
+++ tools/ShapeChecker/check-qm.sh
@@ -5,6 +5,7 @@ build/install/ShapeChecker/bin/ShapeChecker \
 -x http://open-services.net/ns/core\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
 -x http://open-services.net/ns/qm ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
 -x http://open-services.net/ns/qm\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
+-x http://jazz.net/ns/acp ${comment# See https://github.com/oslc-op/oslc-specs/issues/332} \
 -x http://jazz.net/ns/acp\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/332} \
 -v ../../specs/qm/quality-management-vocab.ttl \
 -s ../../specs/qm/quality-management-shapes.ttl
