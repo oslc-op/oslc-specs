abcfe84958bc975283d5e01773ce366f0a26f960
Andrew Berezovskyi <andriib@kth.se>
2020-10-13 23:06:07 +0200

    Disable QM shape checks (#431)

--- tools/ShapeChecker/check-qm.sh
+++ tools/ShapeChecker/check-qm.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 build/install/ShapeChecker/bin/ShapeChecker \
--C ${comment# Enforce check for ResourceShapeConstraints} \
 -t Warning \
+-C ${comment# Enforce check for ResourceShapeConstraints} \
+-q undefinedShape ${comment# TODO remove; see https://github.com/oslc-op/oslc-specs/issues/432} \
 -q unusedVocabulary \
 -x http://open-services.net/ns/core ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
 -x http://open-services.net/ns/core\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
