9dd5b84b02597caec96f161878b72cfe8bbf3695
Andrew Berezovskyi <andriib@kth.se>
2020-10-13 22:36:41 +0200

    Apply Nick's corrections

--- tools/ShapeChecker/check-qm.sh
+++ tools/ShapeChecker/check-qm.sh
@@ -2,6 +2,8 @@
 
 build/install/ShapeChecker/bin/ShapeChecker \
 -t Warning \
+-C ${comment# Enforce check for ResourceShapeConstraints} \
+-q undefinedShape ${comment# TODO remove; see https://github.com/oslc-op/oslc-specs/issues/432} \
 -q unusedVocabulary \
 -x http://open-services.net/ns/core ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
 -x http://open-services.net/ns/core\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/40} \
@@ -11,6 +13,3 @@ build/install/ShapeChecker/bin/ShapeChecker \
 -x http://jazz.net/ns/acp\#.\* ${comment# See https://github.com/oslc-op/oslc-specs/issues/332} \
 -v ../../specs/qm/quality-management-vocab.ttl \
 -s ../../specs/qm/quality-management-shapes.ttl
-
-# TODO reinstate the check
-# -C ${comment# Enforce check for ResourceShapeConstraints} \
