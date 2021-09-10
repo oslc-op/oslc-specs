8daf062596e21a9465f622a3d1ca929eb57a22b3
Andrew Berezovskyi <andriib@kth.se>
2021-09-10 17:35:25 +0200

    Fix problems in the prepared package
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- specs/core/config.js
+++ specs/core/config.js
@@ -12,7 +12,11 @@ var oasisBase = "https://docs.oasis-open-projects.org/oslc-op/core/v3.0/";
 
 var thisBase = wdBase;
 if (specConfig.status != "WD") {
-  thisBase = oasisBase + specConfig.status.toLowerCase() + specConfig.revision + "/";
+  thisBase = oasisBase + specConfig.status.toLowerCase();
+  if(specConfig.status != "OS") {
+    thisBase += specConfig.revision;
+  }
+  thisBase +="/";
 } else {
   // if it's a WD, set
   var scriptUri = document.currentScript.src;
