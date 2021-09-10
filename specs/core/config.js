1bb4884b713a686ba9ca89522b959a00ae16ae85
Andrew Berezovskyi <andriib@kth.se>
2021-09-10 17:41:00 +0200

    Run prettier
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- specs/core/config.js
+++ specs/core/config.js
@@ -13,10 +13,10 @@ var oasisBase = "https://docs.oasis-open-projects.org/oslc-op/core/v3.0/";
 var thisBase = wdBase;
 if (specConfig.status != "WD") {
   thisBase = oasisBase + specConfig.status.toLowerCase();
-  if(specConfig.status != "OS") {
+  if (specConfig.status != "OS") {
     thisBase += specConfig.revision;
   }
-  thisBase +="/";
+  thisBase += "/";
 } else {
   // if it's a WD, set
   var scriptUri = document.currentScript.src;
