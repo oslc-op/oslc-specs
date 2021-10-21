b931ed89e30161f812f1120ba3c8660b89b9f016
Andrew Berezovskyi <andriib@kth.se>
2021-10-21 18:45:17 +0200

    Prepare OSLC Core for OS publication (#536)

--- specs/core/config.js
+++ specs/core/config.js
@@ -1,8 +1,8 @@
 var specConfig = {
-  status: "PS",
-  revision: "02",
-  publishDate: "2021-04-23",
-  previousPublishDate: "2020-09-17",
+  status: "OS",
+  // revision: "02",
+  publishDate: "2021-08-26T12:00Z",
+  previousPublishDate: null,
   previousMaturity: "PS",
 };
 
@@ -12,7 +12,11 @@ var oasisBase = "https://docs.oasis-open-projects.org/oslc-op/core/v3.0/";
 
 var thisBase = wdBase;
 if (specConfig.status != "WD") {
-  thisBase = oasisBase + specConfig.status.toLowerCase() + specConfig.revision + "/";
+  thisBase = oasisBase + specConfig.status.toLowerCase();
+  if (specConfig.status != "OS") {
+    thisBase += specConfig.revision;
+  }
+  thisBase += "/";
 } else {
   // if it's a WD, set
   var scriptUri = document.currentScript.src;
