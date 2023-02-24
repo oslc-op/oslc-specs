72d3584be2428748e67525c5a7398eef9175b01c
Jim Amsden <jamsden@us.ibm.com>
2023-02-24 09:22:56 -0500

    Jra ldm initial (#589)
    * Updates to TRS paging rules
    
    resolves: #578
    
    * Fixing prettier issue
    
    * Minor editorial changes to publish PS02
    
    See  https://issues.oasis-open.org/browse/TCADMIN-4281.
    
    * Introduced new Link Discovery Management Spec

---
+++ specs/ldm/resources/LinkQueryResults.java
@@ -0,0 +1,38 @@
+       /**
+       * Queries the back link index for links pointing to the specified target
+       * URLs scoped to the specified specified global configuration URL and
+       * specified link types. This is a long running operation.
+       *
+        * @param targetURLs
+       *            must specify at least one, but no more than
+       *            {@link ILinkIndexService#MAX_TARGET_URLS}.
+       * @param gcURL
+       *            required
+       * @param linkTypes
+       *            must specify at least one. Valid link type constants are
+       *            defined in {@link ILinkIndexService}.
+       * @return
+       * @throws TeamRepositoryException
+       */
+       public LinkQueryResults queryForLinks(final String[] targetURLs, final String gcURL, final String[] linkTypes) throws TeamRepositoryException;
+ 
+Supporting classes:
+ 
+       public static class LinkQueryResults {
+              // zero based page index
+              public LinkTriple[] links = new LinkTriple[0];
+       }
+      
+       public static class LinkTriple {
+              public String sourceURL;
+              public String linkType;
+              public String targetURL;
+             
+              public LinkTriple() {}
+             
+              public LinkTriple(String sourceURL, String linkType, String targetURL) {
+                     this.sourceURL = sourceURL;
+                     this.linkType = linkType;
+                     this.targetURL = targetURL;
+              }
+       }
